f34446ec92d8fc9a9c349aa7b3f312e9b0919750
Cauã Oliveira <caua.olivergomes22@outlook.com>
2026-09-25 00:40:30 -0300

    Added CommentsDTO

--- src/main/java/com/cauaoliveira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cauaoliveira/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.cauaoliveira.workshopmongo.domain.Post;
 import com.cauaoliveira.workshopmongo.domain.User;
 import com.cauaoliveira.workshopmongo.dto.AuthorDTO;
+import com.cauaoliveira.workshopmongo.dto.CommentDTO;
 import com.cauaoliveira.workshopmongo.repository.PostRepository;
 import com.cauaoliveira.workshopmongo.repository.UserRepository;
 
@@ -36,6 +37,13 @@ public class Instantiation implements CommandLineRunner{
         Post post1 = new Post(null,sdf.parse("21/03/2026"), "Go to the beach", "Bye Bye!",new AuthorDTO(maria));
         Post post2 = new Post(null,sdf.parse("23/03/2026"), "Good Morning!", "Have a great day!",new AuthorDTO(maria));
         
+        CommentDTO c1 = new CommentDTO("Have a nice day!",sdf.parse("21/03/2026"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Have a great day!",sdf.parse("22/03/2026"), new AuthorDTO(alex));
+        CommentDTO c3 = new CommentDTO("Ok, but have a bad day!",sdf.parse("25/03/2026"), new AuthorDTO(bob));
+        
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
         postRepository.saveAll(Arrays.asList(post1,post2));
 
         maria.getPosts().addAll(Arrays.asList(post1,post2));
--- src/main/java/com/cauaoliveira/workshopmongo/domain/User.java
+++ src/main/java/com/cauaoliveira/workshopmongo/domain/User.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -16,8 +15,6 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
-
-    @DBRef(lazy = true)
     private List<Post> posts = new ArrayList<>();    
 
     public User(){
